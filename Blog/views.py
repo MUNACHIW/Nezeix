ef7e926e6702495830e07355255347f1cfbda8a8
MUNACHIW <munasom25@gmail.com>
2024-09-03 22:22:52 +0100

    posdetail query and code

--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 from django.template.loader import render_to_string
 from django.http import HttpResponseNotFound
@@ -18,16 +18,14 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "Blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    try:
-        identified_post = next(post for post in all_posts if post["slug"] == slug)
-        return render(request, "Blog/post-detail.html", {"post": identified_post})
-    except:
-        response = render_to_string("404.html")
-        return HttpResponseNotFound(response)
+    # identified_post = next(post for post in all_posts if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, "Blog/post-detail.html", {"post": identified_post})
 
 
 def notfound(request):
