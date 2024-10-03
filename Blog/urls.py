71efbf59c74276e18535c62f3b5025ba44220e4a
MUNACHIW <munasom25@gmail.com>
2024-10-03 14:40:53 +0100

    implemented more class based views

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
-    path("post", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
+    path("post", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.template.loader import render_to_string
 from django.http import HttpResponseNotFound
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -20,19 +20,38 @@ class StartingPageView(ListView):
         return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "Blog/all-posts.html", {"all_posts": all_posts})
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
 
-def post_detail(request, slug):
-    # identified_post = next(post for post in all_posts if post["slug"] == slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(
-        request,
-        "Blog/post-detail.html",
-        {"post": identified_post, "post_tags": identified_post.tags.all()},
-    )
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "Blog/all-posts.html", {"all_posts": all_posts})
+
+
+class SinglePostView(DetailView):
+    template_name = "Blog/post-detail.html"
+    model = Post
+
+    # for getting correct slug if you are not using id or primary key or detail to also get or access tags
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+
+# def post_detail(request, slug):
+#     # identified_post = next(post for post in all_posts if post["slug"] == slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(
+#         request,
+#         "Blog/post-detail.html",
+#         {"post": identified_post, "post_tags": identified_post.tags.all()},
+#     )
 
 
 def notfound(request):
