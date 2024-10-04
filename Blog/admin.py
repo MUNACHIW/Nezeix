7edccbb7fe886bd6b28b0150d2228a067d7d4c33
MUNACHIW <munasom25@gmail.com>
2024-10-04 19:30:05 +0100

    added styling to my comment and some admin functionalities

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date")
+    list_display = ("title", "date", "author")
+    populated_fields = ("slug", "title")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
