514e82ef9a27502ecb9fc1a72bc4f1a70368710d
MUNACHIW <munasom25@gmail.com>
2024-10-05 21:22:34 +0100

    added read later session and some modifications

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("post", views.AllPostView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    # for read later session
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
