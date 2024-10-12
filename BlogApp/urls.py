f7873e796efc4c658f81cede7e6d5ed6374fa93d
MUNACHIW <munasom25@gmail.com>
2024-10-12 09:21:36 +0100

    fully setup

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -20,9 +20,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Blog.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#  / +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
